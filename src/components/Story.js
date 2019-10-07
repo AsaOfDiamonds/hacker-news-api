d4db0fc489491ef04202fde5f4f3e82079b5739c
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-10-06 19:09:10 -0600

    finshied mapTime.js for posted time, moved selectFields.js to selectors folder

--- src/components/Story.js
+++ src/components/Story.js
@@ -6,6 +6,7 @@ import {
   StoryMeta,
   StoryMetaElement,
 } from "../styles/StoryStyles";
+import { mapTime } from "../mappers/mapTime";
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -28,9 +29,9 @@ export const Story = ({ storyId }) => {
         <span data-testid="story-by">
           <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
         </span>
-        <span data-testid="story-time">
-          <StoryMetaElement color="#000">Posted:</StoryMetaElement>
-          {story.time}
+        <span  data-testid="story-time">
+          <StoryMetaElement color="#000">Posted:</StoryMetaElement> {` `}
+          {mapTime(story.time)}
         </span>
       </StoryMeta>
 
--- src/mappers/mapTime.js
+++ src/mappers/mapTime.js
@@ -1,5 +1,5 @@
 export const mapTime = timestamp => {
-  const seconds = Math.floor((new Date - timestamp * 1000) / 1000);
+  const seconds = Math.floor((new Date() - timestamp * 1000) / 1000);
 
   let interval = Math.floor(seconds / 31536000);
 
